d1decce9a2629000256a887264630191c07fc456
Hector Garcia Rosales <ghectormus@gmail.com>
2023-12-24 23:29:05 -0600

    add third test to take first and last element

--- spec/day_1_spec.rb
+++ spec/day_1_spec.rb
@@ -12,4 +12,10 @@ RSpec.describe Extractor do
     numbers = output.get_numbers("pqr3stu8vwx")
     expect(numbers).to be(38)
   end
+
+  it "obtains 15 from this input a1b2c3d4e5f" do
+    output = Extractor.new
+    numbers = output.get_numbers("a1b2c3d4e5f")
+    expect(numbers).to be(15)
+  end
 end
